4baf3dde3f2c39517082b3f3aec308fe317b2f98
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-08-13 11:25:06 -0500

    completed exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,1 @@
-const message = "Your Webpack application is set up and ready to go. Please start writing code."
-
-document.querySelector("#container").innerHTML = `<h1>${message}</h1>`
-
-console.log(message)
+// see pdf with ERD 
